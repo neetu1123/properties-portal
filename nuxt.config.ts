ae373c1c0d7595b200e6122c90d9f7403f48f5db
neetu1123 <neetuchaurasiya5041@gmail.com>
2025-06-05 01:20:34 +0530

    added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,14 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
 
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/A_Curated_Vacation_logo_page-0001__1_-removebg-preview.png' }
+      ]
+    }
+  },
+
   vite: {
     plugins: [
       tailwindcss(),
